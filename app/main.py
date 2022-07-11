49c7542505459b7ff0081e85715478059055e2ca
Guillermo Riojas <guillermo.riojas@fresha.com>
2022-07-11 23:09:31 +0200

    Splitted Contact model into Production, Defect and DownTime. Updated index route, added POST routes for all models

--- app/main.py
+++ app/main.py
@@ -27,82 +27,83 @@ app.secret_key = 'mysecretkey'
 @app.route('/')
 def index():
 
-    shift_date = datetime.datetime.today().strftime('%Y-%m-%d')
+    # shift_date = datetime.datetime.today().strftime('%Y-%m-%d')
     
-    shift_date_data = get_daily_shift_data(shift_date)
+    # shift_date_data = get_daily_shift_data(shift_date)
 
-    print(json.dumps(shift_date_data, indent=4))
+    # print(json.dumps(shift_date_data, indent=4))
 
-    return render_template('index.html', 
-                            shift_date=shift_date, 
-                            data=shift_date_data['data'], 
-                            rate=list(shift_date_data['rates'].values()), 
-                            goal=list(shift_date_data['goals'].values()), 
-                            reality=list(shift_date_data['reality'].values()),
-                            downtime_labels=list(shift_date_data['downtime'].keys()),
-                            downtime_sum=list(shift_date_data['downtime'].values()),
-                            downtime_colors=shift_date_data['downtime_colors'],
-                            defect_labels=list(shift_date_data['defects'].keys()),
-                            defect_sum=list(shift_date_data['defects'].values()),
-                            defect_colors=shift_date_data['defect_colors'])
+    return 'OK'
+    # return render_template('index.html', 
+    #                         shift_date=shift_date, 
+    #                         data=shift_date_data['data'], 
+    #                         rate=list(shift_date_data['rates'].values()), 
+    #                         goal=list(shift_date_data['goals'].values()), 
+    #                         reality=list(shift_date_data['reality'].values()),
+    #                         downtime_labels=list(shift_date_data['downtime'].keys()),
+    #                         downtime_sum=list(shift_date_data['downtime'].values()),
+    #                         downtime_colors=shift_date_data['downtime_colors'],
+    #                         defect_labels=list(shift_date_data['defects'].keys()),
+    #                         defect_sum=list(shift_date_data['defects'].values()),
+    #                         defect_colors=shift_date_data['defect_colors'])
 
-@app.route('/add_contact', methods=['POST'])
-def add_contact():
+@app.route('/add_production', methods=['POST'])
+def add_production():
     
     if request.method == 'POST':
-        contact = models.Contact(
-            shift_date=datetime.datetime.strptime(request.form['shift_date'],'%Y-%m-%d').date(),
-            shift_hours=request.form['shift_hours'],
-            model=request.form['model'],
-            clip_machine=request.form['clip_machine'],
-            goal=request.form['goal'],
-            rate=request.form['rate'],
-            reality=request.form['reality'],
-            defects=request.form['defects'],
-            defects_qty=request.form['defects_qty'],
-            down_time_reason=request.form['down_time_reason'],
-            down_time_qty=request.form['down_time_qty']
+        production = models.Production(
+          project=request.form['project'],
+          shift_date=datetime.datetime.strptime(request.form['shift_date'],'%Y-%m-%d').date(),
+          shift_time=request.form['shift_time'],
+          machine=request.form['machine'],
+          model=request.form['model'],
+          goal=request.form['goal'],
+          rate=request.form['rate'],
+          quantity=request.form['quantity']
         )   
 
-        app.session.add(contact)
+        app.session.add(production)
         app.session.commit()
-        flash('Contact Added successfully')
+        flash('Production added successfully')
         return redirect(url_for('index'))
 
-@app.route('/update/<id>', methods = ['POST'])
-def update_contact(id):
-    contact = app.session.query(models.Contact).filter_by(id=id).first()
+@app.route('/add_defect', methods=['POST'])
+def add_defect():
+    
+    if request.method == 'POST':
+        defect = models.Defect(
+          project=request.form['project'],
+          shift_date=datetime.datetime.strptime(request.form['shift_date'],'%Y-%m-%d').date(),
+          shift_time=request.form['shift_time'],
+          machine=request.form['machine'],
+          model=request.form['model'],
+          reason=request.form['reason'],
+          quantity=request.form['quantity']            
+        )   
 
-    if request.method == 'GET':
-      return render_template('edit-contact.html', contact = contact)
-
-    elif request.method == 'POST':
-        if request.form['shift_date']: contact.shift_date=datetime.datetime.strptime(request.form['shift_date'],'%Y-%m-%d').date()
-        if request.form['shift_hours']: contact.shift_hours=request.form['shift_hours']
-        if request.form['model']: contact.model=request.form['model']
-        if request.form['clip_machine']: contact.clip_machine=request.form['clip_machine']
-        if request.form['goal']: contact.goal=request.form['goal']
-        if request.form['rate']: contact.rate=request.form['rate']
-        if request.form['reality']: contact.reality=request.form['reality']
-        if request.form['defects']: contact.defects=request.form['defects']
-        if request.form['defects_qty']: contact.defects_qty=request.form['defects_qty']
-        if request.form['down_time_reason']: contact.down_time_reason=request.form['down_time_reason']
-        if request.form['down_time_qty']: contact.down_time_qty=request.form['down_time_qty']
-
-        app.session.add(contact)
+        app.session.add(defect)
         app.session.commit()
-
-        flash('Contact Updated Successfully')
+        flash('Defect added successfully')
         return redirect(url_for('index'))
 
-@app.route('/delete/<string:id>')
-def delete_contact(id):
-    contact = app.session.query(models.Contact).filter_by(id=id).first()
-    app.session.delete(contact)
-    app.session.commit()
+@app.route('/add_downtime', methods=['POST'])
+def add_downtime():
+    
+    if request.method == 'POST':
+        downtime = models.DownTime(
+          project=request.form['project'],
+          shift_date=datetime.datetime.strptime(request.form['shift_date'],'%Y-%m-%d').date(),
+          shift_time=request.form['shift_time'],
+          machine=request.form['machine'],
+          model=request.form['model'],
+          reason=request.form['reason'],
+          quantity=request.form['quantity']            
+        )   
 
-    flash('Contact Removed Successfully')
-    return redirect(url_for('index'))
+        app.session.add(downtime)
+        app.session.commit()
+        flash('Downtime added successfully')
+        return redirect(url_for('index'))
 
 @app.teardown_appcontext
 def remove_session(*args, **kwargs):
