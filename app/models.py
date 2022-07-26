87f43298b65982282ecf6bb3b3e45fb295297eff
Guillermo Riojas <guillermo.riojas@fresha.com>
2022-07-26 21:43:56 +0200

    Added models to support projects, machines, models, defects/downtimes reasons

--- app/models.py
+++ app/models.py
@@ -10,13 +10,24 @@ class Project(Base):
   id = Column(Integer, primary_key=True, index=True)
   name = Column(String(254))
   models = relationship('Model', backref='projects', lazy='select')
+  machines = relationship('Machine', backref='projects', lazy='select')
+  reasons = relationship('Reason', backref='reasons', lazy='select')
+  rate = Column(Integer, default=0)
+  goal = Column(Integer, default=0)
 
   created_at = Column(DateTime, default=datetime.datetime.now())
   updated_at = Column(DateTime, onupdate=datetime.datetime.now())
 
   def to_dict(self):
     dict= { c.name: getattr(self, c.name) for c in self.__table__.columns}
+    dict['created_at'] = dict['created_at'].isoformat() if dict['created_at'] else ''
+    dict['updated_at'] = dict['updated_at'].isoformat() if dict['updated_at'] else ''
+    dict['models'] = [model.to_dict() for model in self.models]
+    dict['machines'] = [machine.to_dict() for machine in self.machines]
+    dict['reasons'] = [reason.to_dict() for reason in self.reasons]
+
     return dict
+
 class Model(Base):
   __tablename__ = 'models'
 
@@ -29,7 +40,47 @@ class Model(Base):
 
   def to_dict(self):
     dict= { c.name: getattr(self, c.name) for c in self.__table__.columns}
+    dict['created_at'] = dict['created_at'].isoformat() if dict['created_at'] else ''
+    dict['updated_at'] = dict['updated_at'].isoformat() if dict['updated_at'] else ''
+
     return dict  
+
+class Machine(Base):
+  __tablename__ = 'machines'
+
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String(254))
+  project_id = Column(Integer, ForeignKey('projects.id'), nullable=False)
+
+  created_at = Column(DateTime, default=datetime.datetime.now())
+  updated_at = Column(DateTime, onupdate=datetime.datetime.now())  
+
+  def to_dict(self):
+    dict= { c.name: getattr(self, c.name) for c in self.__table__.columns}
+    dict['created_at'] = dict['created_at'].isoformat() if dict['created_at'] else ''
+    dict['updated_at'] = dict['updated_at'].isoformat() if dict['updated_at'] else ''
+
+    return dict  
+
+
+class Reason(Base):
+  __tablename__ = 'reasons'
+
+  id = Column(Integer, primary_key=True, index=True)
+  description = Column(String(500))
+  category = Column(String(10)) # defect, downtime
+  project_id = Column(Integer, ForeignKey('projects.id'), nullable=False)
+
+  created_at = Column(DateTime, default=datetime.datetime.now())
+  updated_at = Column(DateTime, onupdate=datetime.datetime.now())  
+
+  def to_dict(self):
+    dict= { c.name: getattr(self, c.name) for c in self.__table__.columns}
+    dict['created_at'] = dict['created_at'].isoformat() if dict['created_at'] else ''
+    dict['updated_at'] = dict['updated_at'].isoformat() if dict['updated_at'] else ''
+
+    return dict
+
 class Production(Base):
   __tablename__ = 'productions'
   
@@ -47,8 +98,11 @@ class Production(Base):
   def to_dict(self):
     dict= { c.name: getattr(self, c.name) for c in self.__table__.columns}
     dict['shift_date']= dict['shift_date'].isoformat()
+    dict['created_at'] = dict['created_at'].isoformat() if dict['created_at'] else ''
+    dict['updated_at'] = dict['updated_at'].isoformat() if dict['updated_at'] else ''
 
     return dict
+
 class Defect(Base):
   __tablename__ ='defects'
 
@@ -69,6 +123,7 @@ class Defect(Base):
     dict= { c.name: getattr(self, c.name) for c in self.__table__.columns}
     dict['shift_date']= dict['shift_date'].isoformat()
     return dict
+
 class DownTime(Base):
   __tablename__ ='downtimes'
 
